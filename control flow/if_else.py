f41359f695532a6d6435377f3f25d662cc1282b9
Raffian-moin <raffianmoin69@gmail.com>
2022-07-13 21:21:10 +0600

    if else

---
+++ control flow/if_else.py
@@ -0,0 +1,15 @@
+age = 26
+
+# if...else
+if (age > 20) :
+    print('true')
+else :
+    print('false')
+
+# if..else if..else
+if (age > 26) :
+    print(age)
+elif (age > 20) :
+    print(f'{age} else if')
+else :
+    print('else if')
