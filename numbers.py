c66f37a47d6bc917c8476767b2ae991cd98a2f3a
moin <moin.uddin@technovista.com.bd>
2022-07-13 15:38:38 +0600

    numbers

---
+++ numbers.py
@@ -0,0 +1,14 @@
+# input
+input1 = input()
+input2 = input()
+ 
+# output
+print(int(input1) + int(input2))
+
+# In python input is always taken as string.
+# So if we need to work with numbers, we have to typecast them.
+
+count = 10_00_000
+print(count)
+
+# Like javascript we can separate digits for readability with underscore
