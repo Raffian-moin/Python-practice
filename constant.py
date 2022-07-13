4e3a0a0cc8cd5bb0fb5022c5cb60e3fe868daa10
Raffian-moin <raffianmoin69@gmail.com>
2022-07-13 21:14:08 +0600

    type conversion

---
+++ constant.py
@@ -0,0 +1,8 @@
+from lib2to3.pgen2.token import NUMBER
+
+
+NUMBER = 12
+
+NUMBER = 20
+
+print(NUMBER);
---
+++ type_conversion.py
@@ -0,0 +1,13 @@
+name = "moin"
+age = 20
+is_awesome = True
+
+print(type(name))
+print(type(age))
+print(type(is_awesome))
+
+print(bool(name))
+print(str(age))
+print(int(is_awesome))
+
+# use type to see the data type in python
