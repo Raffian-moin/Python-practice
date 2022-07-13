28aec9e79182ea0b048b54686cbe07da7a8a65ed
Raffian-moin <raffianmoin69@gmail.com>
2022-07-13 20:07:21 +0600

    string

---
+++ string.py
@@ -0,0 +1,17 @@
+message = "you are awesome";
+new_message = 'hELLO, "you are awesome"';
+
+# Escaping in  python
+escape_character = "Hello, I\'m Moin";
+
+# another way of escaping in python
+new_escape_character = r"Hello, I'm Moin"
+
+print(message);
+print(new_message);
+print(escape_character);
+print(new_escape_character);
+
+# string length
+print(len(new_escape_character))
+
