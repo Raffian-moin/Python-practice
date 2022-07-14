9105585c4a17001f015649cc3626f619fe3d60d9
moin <moin.uddin@technovista.com.bd>
2022-07-14 12:33:45 +0600

    loop

---
+++ control flow/loop.py
@@ -0,0 +1,27 @@
+age = 10
+
+# while (age < 100): 
+#     print(age)
+#     age += 1
+
+# break statement
+
+# while (age < 40) :
+#     print(age)
+#     age += 1
+#     if (age == 30):
+#         break
+
+# continue statement
+
+while (age < 40) :
+    age += 1
+    if not (age % 2) :
+        continue
+    print(age)
+
+# pass
+if (age == 10) : 
+    pass
+
+# pass is like return 0 or false in python
