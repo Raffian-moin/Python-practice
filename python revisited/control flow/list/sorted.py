29ca51bb36575c0eeff62c2cd0ee97d51b250bde
Raffian-moin <raffianmoin69@gmail.com>
2022-11-09 11:18:46 +0600

    sorted

---
+++ python revisited/control flow/list/sorted.py
@@ -0,0 +1,11 @@
+# do not modify the original list. modify and return
+
+numbers = [2, 5, 4, 9, 7]
+
+sorted_numbers = sorted(numbers)
+
+print(sorted_numbers)
+
+sorted_numbers = sorted(numbers, reverse=True)
+
+print(sorted_numbers)
