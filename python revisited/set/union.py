589619b11403be7bddedd618e55c58d0efcb6370
Raffian-moin <raffianmoin69@gmail.com>
2022-11-11 18:50:18 +0600

    set union

---
+++ python revisited/set/union.py
@@ -0,0 +1,14 @@
+language1 = {'c', 'python'}
+language2 = {'c', 'html'}
+language3 = {'css', 'php'}
+
+language_union = language1.union(language2).union(language3)
+print(language_union)
+
+language_union2 = language1 | language2
+print(language_union2)
+
+# set() vs pipe(|) operator
+
+# set accepts sets, lists, dictoary and converts them in set and then does the union
+# but pipe only accespt set
