2db2b244bfb61ed80f8817711fda4e63ee3b7936
Raffian-moin <raffianmoin69@gmail.com>
2022-11-14 19:45:07 +0600

    init

---
+++ python revisited/oop/init.py
@@ -0,0 +1,8 @@
+class MyClass:
+    def __init__(self, name, age) -> None:
+        self.amar_anme = name
+        self.amar_age = age
+
+my_class = MyClass('moin', '23')
+
+print(my_class.amar_age)
