01a2a650f41f3cabb1abadbc93e525e2629ec6cf
Raffian-moin <raffianmoin69@gmail.com>
2023-07-08 21:23:43 +0600

    String introduction

---
+++ string/string.py
@@ -0,0 +1,11 @@
+# String with variable
+s='new string'
+
+# Using variable inside a string
+print(f"{s} inside old string")
+
+# Escaping ' we use backslash
+print('Hello I\'m Moin')
+
+#String concatenation
+print('Is this '+ s+ '?')
