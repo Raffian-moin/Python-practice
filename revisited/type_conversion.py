16c65e1f008cdec00298f2a34bf3c3360eb2d3f1
Raffian-moin <raffianmoin69@gmail.com>
2023-07-20 11:02:28 +0600

    type conversion

---
+++ revisited/type_conversion.py
@@ -0,0 +1,22 @@
+"""
+to know the type of a variable we use type() method.
+to convert to integer use int()
+to convert to float use float()
+to convert to boolean use bool()
+to convert to string use str()
+"""
+
+st="string"
+integer=12
+fl=0.12
+bl=True
+
+print(type(st))
+print(type(integer))
+print(type(fl))
+print(type(bl))
+
+print(int(fl))
+print(float(integer))
+print(str(integer))
+print(bool(fl))
