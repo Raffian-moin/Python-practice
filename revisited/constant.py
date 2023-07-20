506bb805358369113b9ed02facf2298643a21752
Raffian-moin <raffianmoin69@gmail.com>
2023-07-20 10:52:18 +0600

    constant

---
+++ revisited/constant.py
@@ -0,0 +1,9 @@
+"""
+Python doesn't have constant. so if we want to use constant we declare a variable
+naming all uppercase. thus we know that it's a constant. but remember it is mutable.
+we just treat it as constant
+
+""" 
+
+PI=3.1416
+print(PI)
